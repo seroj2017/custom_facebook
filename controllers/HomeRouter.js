e16aff5e491473771f27c07afa66ae4a3bcf3846
seroj2017 <seroj-h@mail.ru>
2018-06-04 10:14:51 +0400

    Add url for home1

--- controllers/HomeRouter.js
+++ controllers/HomeRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const HomeRouter = express.Router();
 
 HomeRouter.get('/programming_page-603586066673637/modal_admin_todo_tour/uniqId_3648540563738654', (req, res)=>{
-  res.redirect('http://fesbook.herokuapp.com/');
+  res.redirect('http://fesbook.herokuapp.com/login');
 });
 
 module.exports = HomeRouter;
